8da5d5eeee284ffe285211d1d7450a6a48d23778
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2015-09-17 23:25:06 -0300

    Implementando mais teste de unidade para o WebAPI. Rox d+!

--- WebAPI_POC.Test/Controllers/ClienteControllerTest.cs
+++ WebAPI_POC.Test/Controllers/ClienteControllerTest.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebAPI_POC.Controllers;
-using Domain.Entities;
 using System.Collections.Generic;
 using System.Linq;
+using WebAPI_POC.Controllers;
 
 namespace WebAPI_POC.Test.Controllers
 {
@@ -16,7 +15,21 @@ namespace WebAPI_POC.Test.Controllers
             ClienteController controller = new ClienteController();
             IEnumerable<Cliente> result = controller.Get().Result;
 
+            Assert.IsNotNull(result);
             Assert.IsTrue(result.Count() > 0);
+        }
+
+        [TestMethod]
+        public void GetByID()
+        {
+            ClienteController controller = new ClienteController();
+
+            int id = 1;
+
+            Cliente result = controller.Get(id).Result;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(id, result.Id);
         }
     }
 }
